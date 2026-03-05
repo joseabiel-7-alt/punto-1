8fc7dfb5398cbac7e135d2ead12d0ecd0af7c214
joseabiel-7-alt <jose_abiel-7@hotmail.com>
2026-03-05 11:21:36 -0500

    punto 1

--- principal.java
+++ principal.java
@@ -6,20 +6,33 @@ public class principal {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int opt = 1;
-        //boolean nw = true;
+       
         metodos m = new metodos();
+        
+        System.out.println("Ingrese numero de filas:");        
+        int filas = sc.nextInt();
+        System.out.println("Ingrese numero de columnas:"); 
+        int columnas = sc.nextInt();
 
-        obj[][] almacen = m.llenarMatriz(2, 2, sc);
+
+        obj[][] almacen = m.llenarMatriz(filas, columnas, sc);
         System.out.print("Ingrese el nombre del producto a buscar: ");
         String nombreBuscado = sc.next();
         m.buscarPorNombre(almacen, nombreBuscado);
 
-        sc.close();
+        
         while (opt == 1) {
             System.out.println("Ingrese el nombre de otro producto: 1) si 2) no: ");
             opt = sc.nextInt();
+            if (opt == 1) {
+                System.out.print("Ingrese el nombre del producto a buscar: ");
+                nombreBuscado = sc.next();
+                m.buscarPorNombre(almacen, nombreBuscado);
+            }
+            opt = sc.nextInt();
+
 
         }
-
+sc.close();
     }
 }
